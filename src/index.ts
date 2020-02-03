a5b4f8dd846ca8cf937c234aca279e3bc30a4c94
parunkov <parunkov@gmail.com>
2020-02-03 16:21:10 +0500

    Сделал для вертикального

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import {slider1} from './slider-1.ts';
 const sliders = document.querySelectorAll('.ts-slider__container');
 // console.log(typeof sliders);
 slider1(sliders[0], false, true, true);
-slider1(sliders[1], true, true, false);
+slider1(sliders[1], true, true, true);
 
