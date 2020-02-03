571bf4b7b34aa24b89fcf80834aa6b0bbdb1cd68
parunkov <parunkov@gmail.com>
2020-02-03 09:08:29 +0500

    Странное поведение теста

--- test/slider-move.test.js
+++ test/slider-move.test.js
@@ -1,41 +1,9 @@
 import {$} from '../node_modules/jquery/dist/jquery.js';
-// import {moveToggle, left} from '../src/slider-move.ts';
+import {Toggle} from '../src/slider-move.ts';
 
-// let left;
-// function moveToggle(toggle, min, max) {
-// 	const onMouseDown = (evt) => {
-// 		evt.preventDefault();
-// 		let startCoordX = evt.clientX;
-// 		let toggleLeft;
-
-// 		const onMouseMove = (moveEvt) => {
-// 			moveEvt.preventDefault();
-// 			let shift = startCoordX - moveEvt.clientX;
-// 			startCoordX = moveEvt.clientX;
-// 			toggleLeft = toggle.offsetLeft - shift;
-// 			if (toggleLeft < min) {
-// 				toggleLeft = min;
-// 			} else if (toggleLeft > max) {
-// 				toggleLeft = max;
-// 			} 
-// 			toggle.style.left = `${toggleLeft}px`;
-// 			left = toggleLeft;
-// 			console.log(left);
-// 		}
-// 		const onMouseUp = (upEvt) => {
-// 			upEvt.preventDefault();
-// 			document.removeEventListener('mouseup', onMouseUp);
-// 			document.removeEventListener('mousemove', onMouseMove);
-// 		}
-// 		document.addEventListener('mousemove', onMouseMove);
-// 		document.addEventListener('mouseup', onMouseUp);
-// 	}
-// 	toggle.addEventListener('mousedown', onMouseDown);
-// }
-
-let toggle = setFixtures();
-toggle.offsetLeft = 0;
-toggle.pageX = 0;
+let toggleDiv = setFixtures();
+toggleDiv[0].pageX = 100;
+toggleDiv[0].pageY = 100;
 const addMethodAddEventListener = (obj) => {
 	obj.addEventListener = (evt, arg) => {
 		if (evt === 'mousedown') {
@@ -47,57 +15,114 @@ const addMethodAddEventListener = (obj) => {
 		}
 	}
 }
-addMethodAddEventListener(toggle);
+addMethodAddEventListener(toggleDiv);
 
-// toggle.addEventListener('mousedown', function() {
-// 	console.log('addEventListener добавлен');
-// });
+let toggle = new Toggle(toggleDiv[0], 0, 300, 200, false);
 
-const min = 0;
-const max = 100;
-
-// moveToggle(toggle, 0, 100);
-// console.log(moveToggle);
-// console.log(left);
-// console.log(toggle.pageX);
-console.log(toggle.mousemove.pageX);
-toggle.trigger('mousemove', toggle.pageX = 100);
-// console.log(toggle.trigger('mousemove'));
-// console.log(toggle.pageX);
-
-describe('Toggle', function() {
-	it('mousedown', function() {
-		spyOnEvent(toggle, 'mousedown');
-		toggle.addEventListener('mousedown', (evt) => {
-			evt.preventDefault;
-			// console.log('!!!');
-			// console.log(evt.clientX);
-			// for (let key in evt) {
-			// 	console.log(key);
-			// }
-			let startCoordX = evt.clientX;
-			console.log(startCoordX);
-			let toggleLeft;
-		});
-		toggle.trigger('mousedown');
-		expect('mousedown').toHaveBeenTriggeredOn(toggle);
+describe('setStyle', function() {
+	it('horizontal', function() {
+		toggleDiv[0].style.left = '';
+		toggleDiv[0].style.top = '';
+		toggle.setStyle();
+		expect(toggleDiv[0].style.left).toBe('200px');
+		expect(toggleDiv[0].style.top).toBe('');
 	});
-	it('mousemove', function() {
-		spyOnEvent(toggle, 'mousemove');
-		toggle.trigger('mousemove');
-		expect('mousemove').toHaveBeenTriggeredOn(toggle);
+	it('vertical', function() {
+		toggleDiv[0].style.left = '';
+		toggleDiv[0].style.top = '';
+		toggle.vertical = true;
+		toggle.setStyle();
+		expect(toggleDiv[0].style.left).toBe('');
+		expect(toggleDiv[0].style.top).toBe('200px');
 	});
-	// it('mousemove', function() {
-	// 	spyOnEvent(toggle, 'mousedown');
-	// 	spyOnEvent(toggle, 'mousemove');
-	// 	moveToggle(toggle, 0, 100);
-	// 	toggle.trigger('mousedown');
-	// 	toggle.trigger('mousemove', toggle.pageX = 50);
-	// 	expect(toggle.pageX).toEqual(50);
-	// 	// moveToggle(toggle, 0, 100);
-	// 	console.log(left);
-	// 	console.log(moveToggle(toggle, 0, 100));
-	// 	// expect(left).toEqual(50);
-	// });
 });
+
+// // toggleDiv.trigger('mousemove', toggleDiv[0].pageX = 200,  toggleDiv[0].pageY = 200);
+
+// // import {moveToggle, left} from '../src/slider-move.ts';
+
+// // let left;
+// // function moveToggle(toggle, min, max) {
+// // 	const onMouseDown = (evt) => {
+// // 		evt.preventDefault();
+// // 		let startCoordX = evt.clientX;
+// // 		let toggleLeft;
+
+// // 		const onMouseMove = (moveEvt) => {
+// // 			moveEvt.preventDefault();
+// // 			let shift = startCoordX - moveEvt.clientX;
+// // 			startCoordX = moveEvt.clientX;
+// // 			toggleLeft = toggle.offsetLeft - shift;
+// // 			if (toggleLeft < min) {
+// // 				toggleLeft = min;
+// // 			} else if (toggleLeft > max) {
+// // 				toggleLeft = max;
+// // 			} 
+// // 			toggle.style.left = `${toggleLeft}px`;
+// // 			left = toggleLeft;
+// // 			console.log(left);
+// // 		}
+// // 		const onMouseUp = (upEvt) => {
+// // 			upEvt.preventDefault();
+// // 			document.removeEventListener('mouseup', onMouseUp);
+// // 			document.removeEventListener('mousemove', onMouseMove);
+// // 		}
+// // 		document.addEventListener('mousemove', onMouseMove);
+// // 		document.addEventListener('mouseup', onMouseUp);
+// // 	}
+// // 	toggle.addEventListener('mousedown', onMouseDown);
+// // }
+
+
+// // toggle.addEventListener('mousedown', function() {
+// // 	console.log('addEventListener добавлен');
+// // });
+
+// // const min = 0;
+// // const max = 100;
+
+// // moveToggle(toggle, 0, 100);
+// // console.log(moveToggle);
+// // console.log(left);
+// // console.log(toggle.pageX);
+// // console.log(toggleDiv[0].mousemove.pageX);
+// toggleDiv.trigger('mousemove', toggleDiv.pageX = 100);
+// // console.log(toggle.trigger('mousemove'));
+// // console.log(toggle.pageX);
+
+// describe('Toggle', function() {
+// 	it('mousedown', function() {
+// 		spyOnEvent(toggleDiv, 'mousedown');
+// 		toggleDiv.addEventListener('mousedown', (evt) => {
+// 			evt.preventDefault;
+// 			// console.log('!!!');
+// 			// console.log(evt.clientX);
+// 			// for (let key in evt) {
+// 			// 	console.log(key);
+// 			// }
+// 			let startCoordX = evt.clientX;
+// 			// console.log(startCoordX);
+// 			let toggleLeft;
+// 		});
+// 		toggleDiv.trigger('mousedown');
+// 		expect('mousedown').toHaveBeenTriggeredOn(toggleDiv);
+// 	});
+// 	it('mousemove', function() {
+// 		spyOnEvent(toggleDiv, 'mousemove');
+// 		toggleDiv.trigger('mousemove');
+// 		expect('mousemove').toHaveBeenTriggeredOn(toggleDiv);
+// 	});
+// 	// it('mousemove', function() {
+// 	// 	spyOnEvent(toggle, 'mousedown');
+// 	// 	spyOnEvent(toggle, 'mousemove');
+// 	// 	moveToggle(toggle, 0, 100);
+// 	// 	toggle.trigger('mousedown');
+// 	// 	toggle.trigger('mousemove', toggle.pageX = 50);
+// 	// 	expect(toggle.pageX).toEqual(50);
+// 	// 	// moveToggle(toggle, 0, 100);
+// 	// 	console.log(left);
+// 	// 	console.log(moveToggle(toggle, 0, 100));
+// 	// 	// expect(left).toEqual(50);
+// 	// });
+// });
 
