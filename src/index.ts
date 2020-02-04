c1b0a90a2c6a728ead8bdccac3cc3fbbaeffc685
parunkov <parunkov@gmail.com>
2020-02-04 15:13:36 +0500

    вертикальный слайдер

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import {slider1} from './slider-1.ts';
 import {initControl} from './init-control.ts';
 
-initControl();
+initControl('#ts-slider__wrap-1',false);
+initControl('#ts-slider__wrap-2',true);
 
 const sliders = document.querySelectorAll('.ts-slider__container');
 // console.log(typeof sliders);
 slider1(sliders[0], false, true, true);
 slider1(sliders[1], true, true, true);
-slider1(sliders[2], false, true, true);
+// slider1(sliders[2], false, true, true);
 
--- src/init-control.ts
+++ src/init-control.ts
@@ -1,4 +1,4 @@
-export function initControl() {
+export function initControl(wrap: string, vertical: boolean) {
 	const contolMarkup: string = `<div class="ts-slider__container">
           <div class="ts-slider__bar">
             <div class="ts-slider__toggle ts-slider__toggle--min">
@@ -10,6 +10,14 @@ export function initControl() {
             <div class="ts-slider__range"></div>
           </div>
         </div>`;
-	const controlWrap: HTMLElement = document.querySelector('#ts-slider__wrap');
+	const controlWrap: HTMLElement = document.querySelector(wrap);
 	controlWrap.innerHTML = contolMarkup;
+  const div = controlWrap.querySelectorAll('div');
+  if (vertical) {
+    for (let i = 0; i < div.length; i++) {
+      const firstClass: string = div[i].classList[0];
+      const verticalClass: string = firstClass + '--vertical';
+      div[i].classList.add(verticalClass);
+    }
+  }
 }
