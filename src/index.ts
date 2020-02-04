752ab87b74a54cecf9c06408cae41bf68c1548c7
parunkov <parunkov@gmail.com>
2020-02-04 16:50:29 +0500

    Сделал горизонтальную шкалу

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {slider1} from './slider-1.ts';
 import {initControl} from './init-control.ts';
+import {Scale} from './scale.ts';
 
 initControl('#ts-slider__wrap-1',false);
 initControl('#ts-slider__wrap-2',true);
@@ -9,4 +10,5 @@ const sliders = document.querySelectorAll('.ts-slider__container');
 slider1(sliders[0], false, true, true);
 slider1(sliders[1], true, true, true);
 // slider1(sliders[2], false, true, true);
+const scale = new Scale(sliders[0], 6, false);
 
---
+++ src/scale.ts
@@ -0,0 +1,27 @@
+export class Scale {
+
+	slider: HTMLElement;
+	quantity: number;
+	isVertical: boolean;
+
+	constructor(slider, quantity, isVertical) {
+		this.slider = slider;
+		this.quantity = quantity;
+		this.isVertical = isVertical;
+		this.createItem();
+	}
+
+	createItem() {
+		const bar = this.slider.querySelector('.ts-slider__bar');
+		console.log(bar.offsetWidth);
+		for (let i = 0; i < this.quantity; i++) {
+			console.log(bar.offsetWidth / (i + 1));
+			const scaleItem = document.createElement('div');
+			scaleItem.classList.add('ts-slider__scale-item');
+			scaleItem.style.left = `${bar.offsetWidth / (this.quantity - 1) * i}px`;
+			scaleItem.textContent = `${bar.offsetWidth / (this.quantity - 1) * i}`;
+			bar.append(scaleItem);
+		}
+	}
+
+}
