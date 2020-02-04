6b1f954bbf6b5338fb42129ac86c0929a5436a6d
parunkov <parunkov@gmail.com>
2020-02-04 13:04:16 +0500

    Вставка HTML

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import {slider1} from './slider-1.ts';
+import {initControl} from './init-control.ts';
+
+initControl();
 
 const sliders = document.querySelectorAll('.ts-slider__container');
 // console.log(typeof sliders);
 slider1(sliders[0], false, true, true);
 slider1(sliders[1], true, true, true);
+slider1(sliders[2], false, true, true);
 
---
+++ src/init-control.ts
@@ -0,0 +1,15 @@
+export function initControl() {
+	const contolMarkup: string = `<div class="ts-slider__container">
+          <div class="ts-slider__bar">
+            <div class="ts-slider__toggle ts-slider__toggle--min">
+              <div class="ts-slider__toggle-value ts-slider__toggle-value--min"></div>
+            </div>
+            <div class="ts-slider__toggle ts-slider__toggle--max">
+              <div class="ts-slider__toggle-value ts-slider__toggle-value--max"></div>
+            </div>
+            <div class="ts-slider__range"></div>
+          </div>
+        </div>`;
+	const controlWrap: HTMLElement = document.querySelector('#ts-slider__wrap');
+	controlWrap.innerHTML = contolMarkup;
+}
