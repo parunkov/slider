062ec92a7013f60898612b44d241b9ebc6903f56
parunkov <parunkov@gmail.com>
2020-02-06 10:55:45 +0500

    Сделал проценты

--- src/set-control-precent.ts
+++ src/set-control-precent.ts
@@ -1,21 +1,19 @@
 import {Toggle} from './toggle.ts';
-import {valueTab} from './value-tab.ts';
 
-export function setControlPrecent(slider, isVertical: boolean, isRange: boolean, isTab: boolean) {
-	interface Precent {
-		min: number;
-		max: number;
-	}
-	let precent: Precent = {
-		min: 0,
-		max: 0
-	};
+interface Precent {
+	min: number;
+	max: number;
+}
+let precent: Precent = {
+	min: 0,
+	max: 0
+};
+
+function setControlPrecent(slider, isVertical: boolean, isRange: boolean, isTab: boolean) {
 	window.addEventListener('load', (windowLoadEvt) => {
 		windowLoadEvt.preventDefault();
 		const minToggleElem: HTMLElement = slider.querySelector('.ts-slider__toggle--min');
 		const maxToggleElem: HTMLElement = slider.querySelector('.ts-slider__toggle--max');
-		// const minTabElem: HTMLElement = slider.querySelector('.ts-slider__toggle-value--min');
-		// const maxTabElem: HTMLElement = slider.querySelector('.ts-slider__toggle-value--max');
 		const range: HTMLElement = slider.querySelector('.ts-slider__range');
 		const barElem: HTMLElement = slider.querySelector('.ts-slider__bar');
 		const barWidth: number = barElem.offsetWidth;
@@ -24,16 +22,6 @@ export function setControlPrecent(slider, isVertical: boolean, isRange: boolean,
 		let toggleMaxOffset;
 		let toggleMinOffset;
 		const setPrecent = (value) => value / barMax * 100;
-		// interface Precent {
-		// 	minToggleElem: number;
-		// 	maxToggleElem: number;
-		// }
-		// let precent: Precent;
-
-		// if (!isTab) {
-		// 	minTabElem.hidden = true;
-		// 	maxTabElem.hidden = true;
-		// }
 
 		if (!isRange) {
 			minToggleElem.hidden = true;
@@ -53,8 +41,6 @@ export function setControlPrecent(slider, isVertical: boolean, isRange: boolean,
 		let toggleMin = new Toggle(minToggleElem, 0, toggleMaxOffset, toggleMinOffset, isVertical);
 		toggleMax.moveToggle();
 		toggleMin.moveToggle();
-		// let minValueTab = new valueTab(minTabElem, toggleMin.value);
-		// let maxValueTab = new valueTab(maxTabElem, toggleMax.value);
 
 		if (!isRange) {
 			toggleMin.value = 0;
@@ -70,14 +56,10 @@ export function setControlPrecent(slider, isVertical: boolean, isRange: boolean,
 				range.style.left = `${toggleMin.value}px`;
 				range.style.width = `${(toggleMax.value - toggleMin.value)}px`;
 			}
-			// minValueTab.value = toggleMin.value;
-			// maxValueTab.value = toggleMax.value;
-			// minValueTab.setValue();
-			// maxValueTab.setValue();
 		}
 		setRanre();
 
-		const onMouseDown = (evt) => {
+		const onMouseDownCoincidence = (evt) => {
 			evt.preventDefault();
 			let coincidenceToggle: boolean = false;
 			maxToggleElem.hidden = true;
@@ -122,21 +104,31 @@ export function setControlPrecent(slider, isVertical: boolean, isRange: boolean,
 			document.addEventListener('mouseup', onMouseUp);
 		}
 
-		maxToggleElem.addEventListener('mousedown', onMouseDown);
-		// minToggleElem.addEventListener('mousedown', onMouseDown);
-		// document.addEventListener('mousedown', () => {
-		// });
+		maxToggleElem.addEventListener('mousedown', onMouseDownCoincidence);
 
 
-		document.addEventListener('mousemove', () => {
-			setRanre();
-			precent.min = setPrecent(toggleMin.value);
-			precent.max = setPrecent(toggleMax.value);
-			console.log(precent);
-		});
+		const onMouseDownPrecent = (evt) => {
+			evt.preventDefault();
+			
+			const onMouseMove = (moveEvt) => {
+				moveEvt.preventDefault();
+				setRanre();
+				precent.min = setPrecent(toggleMin.value);
+				precent.max = setPrecent(toggleMax.value);
+				// console.log(precent);
+			}
+			const onMouseUp = (upEvt) => {
+				upEvt.preventDefault();
+				document.removeEventListener('mouseup', onMouseUp);
+				document.removeEventListener('mousemove', onMouseMove);
+			}
+			document.addEventListener('mousemove', onMouseMove);
+			document.addEventListener('mouseup', onMouseUp);
+		}
 
+		maxToggleElem.addEventListener('mousedown', onMouseDownPrecent);
+		minToggleElem.addEventListener('mousedown', onMouseDownPrecent);
 	});
-// console.log(precent);
-// return precent;
 }
 
+export {precent, setControlPrecent};
--- src/view.ts
+++ src/view.ts
@@ -1,6 +1,6 @@
 import {initControl} from './init-control.ts';
 import {Scale} from './scale.ts';
-import {setControlPrecent} from './set-control-precent.ts';
+import {precent, setControlPrecent} from './set-control-precent.ts';
 import {valueTab} from './value-tab.ts';
 import {SliderData} from './data.ts';
 import {Toggle} from './toggle.ts';
@@ -10,126 +10,13 @@ interface ViewData {
 	max: number;
 }
 
-// export class View {
-
-// 	data: SliderData;
-// 	container: HTMLElement;
-// 	minToggleElem: HTMLElement;
-// 	maxToggleElem: HTMLElement;
-// 	minTabElem: HTMLElement;
-// 	maxTabElem: HTMLElement;
-// 	rangeElem: HTMLElement;
-// 	barElem: HTMLElement;
-// 	viewData: ViewData;
-
-// 	constructor(data) {
-// 		this.data = data;
-// 		this.initView();
-// 		this.addListener();
-// 		this.addScale();
-// 		console.log(this);
-// 	}
-
-// 	createElements() {
-// 		this.container = document.querySelector(this.data.wrapId).querySelector('.ts-slider__container');
-// 		// this.minTab = this.sliderContainer.querySelector('.ts-slider__toggle-value--min');
-// 		// this.maxTab = this.sliderContainer.querySelector('.ts-slider__toggle-value--max');
-
-// 		this.minToggleElem = this.container.querySelector('.ts-slider__toggle--min');
-// 		this.maxToggleElem = this.container.querySelector('.ts-slider__toggle--max');
-// 		this.rangeElem = this.container.querySelector('.ts-slider__range');
-// 		this.barElem = this.container.querySelector('.ts-slider__bar');
-
-// 		if (!this.data.isTab) {
-// 			this.minTabElem.hidden = true;
-// 			this.maxTabElem.hidden = true;
-// 		}
-
-// 		if (!this.data.isRange) {
-// 			this.minToggleElem.hidden = true;
-// 		}
-// 	}
-
-// 	initView() {
-// 		initControl(this.data.wrapId, this.data.isVertical);
-// 		this.createElements();
-
-// 		const barWidth: number = this.barElem.offsetWidth;
-// 		const barHeight: number = this.barElem.offsetHeight;
-// 		let barMax;
-// 		let toggleMaxOffset;
-// 		let toggleMinOffset;
-// 		const setPrecent = (value) => value / barMax * 100;
-
-// 		if (this.data.isVertical) {
-// 			barMax = barHeight;
-// 			toggleMaxOffset = this.maxToggleElem.offsetTop;
-// 			toggleMinOffset = this.minToggleElem.offsetTop;
-// 		} else {
-// 			barMax = barWidth;
-// 			toggleMaxOffset = this.maxToggleElem.offsetLeft;
-// 			toggleMinOffset = this.minToggleElem.offsetLeft;
-// 		}
-
-// 		let toggleMax = new Toggle(this.maxToggleElem, toggleMinOffset, barMax, toggleMaxOffset, this.data.isVertical);
-// 		let toggleMin = new Toggle(this.minToggleElem, 0, toggleMaxOffset, toggleMinOffset, this.data.isVertical);
-// 		toggleMax.moveToggle();
-// 		toggleMin.moveToggle();
-// 		// let minValueTab = new valueTab(this.minTabElem, toggleMin.value);
-// 		// let maxValueTab = new valueTab(this.maxTabElem, toggleMax.value);
-
-// 		if (!this.data.isRange) {
-// 			toggleMin.value = 0;
-// 		}
-
-// 		const setRanre = () => {
-// 			toggleMax.min = toggleMin.value;
-// 			toggleMin.max = toggleMax.value;
-// 			if (this.data.isVertical) {
-// 				this.rangeElem.style.top = `${toggleMin.value}px`;
-// 				this.rangeElem.style.height = `${(toggleMax.value - toggleMin.value)}px`;
-// 			} else {
-// 				this.rangeElem.style.left = `${toggleMin.value}px`;
-// 				this.rangeElem.style.width = `${(toggleMax.value - toggleMin.value)}px`;
-// 			}
-// 			// minValueTab.value = toggleMin.value;
-// 			// maxValueTab.value = toggleMax.value;
-// 			// minValueTab.setValue();
-// 			// maxValueTab.setValue();
-// 		}
-// 		setRanre();
-
-// 	}
-// 	addListener() {
-// 		// let precent = setControlPrecent(this.container, this.data.isVertical, this.data.isRange, this.data.isTab);
-
-
-// 		// let minValueTab = new valueTab(this.minTab, this.data.minToggleValue);
-// 		// let maxValueTab = new valueTab(this.maxTab, this.data.maxToggleValue);
-// 		document.addEventListener('mousemove', () => {
-// 			// minValueTab.value = precent.minToggle;
-// 			// maxValueTab.value = precent.maxToggle;
-// 			// minValueTab.setValue();
-// 			// maxValueTab.setValue();
-// 			// console.log(precent);
-// 		}) ;
-// 	}
-// 	addScale() {
-// 		if (this.data.isScale) {
-// 			const scale = new Scale(this.container, this.data.minValue, this.data.maxValue, this.data.scaleQuantity, this.data.isVertical);
-// 		}
-// 	}
-// 	// data() {
-// 	// 	console.log(this.data);
-// 	// }
-// }
-
-
+// let viewData: ViewData = precent;
 
 export class View {
 
 	data: SliderData;
-	sliderContainer: HTMLElement;
+	viewData: ViewData;
+	container: HTMLElement;
 	minTab: HTMLElement;
 	maxTab: HTMLElement;
 
@@ -142,28 +29,33 @@ export class View {
 
 	initView() {
 		initControl(this.data.wrapId, this.data.isVertical);
-		this.sliderContainer = document.querySelector(this.data.wrapId).querySelector('.ts-slider__container');
-		this.minTab = this.sliderContainer.querySelector('.ts-slider__toggle-value--min');
-		this.maxTab = this.sliderContainer.querySelector('.ts-slider__toggle-value--max');
+		this.container = document.querySelector(this.data.wrapId).querySelector('.ts-slider__container');
+		this.minTab = this.container.querySelector('.ts-slider__toggle-value--min');
+		this.maxTab = this.container.querySelector('.ts-slider__toggle-value--max');
 	}
 	addListener() {
-		let precent = setControlPrecent(this.sliderContainer, this.data.isVertical, this.data.isRange, this.data.isTab);
-		// let minValueTab = new valueTab(this.minTab, this.data.minToggleValue);
-		// let maxValueTab = new valueTab(this.maxTab, this.data.maxToggleValue);
-		document.addEventListener('mousemove', () => {
-			// minValueTab.value = precent.minToggle;
-			// maxValueTab.value = precent.maxToggle;
-			// minValueTab.setValue();
-			// maxValueTab.setValue();
-			// console.log(precent);
-		}) ;
+		setControlPrecent(this.container, this.data.isVertical, this.data.isRange, this.data.isTab);
+		const onMouseDown = (evt) => {
+			evt.preventDefault();
+			
+			const onMouseMove = (moveEvt) => {
+				this.viewData = precent;
+				// console.log(this.viewData);
+			}
+			const onMouseUp = (upEvt) => {
+				upEvt.preventDefault();
+				document.removeEventListener('mouseup', onMouseUp);
+				document.removeEventListener('mousemove', onMouseMove);
+			}
+			document.addEventListener('mousemove', onMouseMove);
+			document.addEventListener('mouseup', onMouseUp);
+		}
+
+		document.addEventListener('mousedown', onMouseDown);
 	}
 	addScale() {
 		if (this.data.isScale) {
-			const scale = new Scale(this.sliderContainer, this.data.minValue, this.data.maxValue, this.data.scaleQuantity, this.data.isVertical);
+			const scale = new Scale(this.container, this.data.minValue, this.data.maxValue, this.data.scaleQuantity, this.data.isVertical);
 		}
 	}
-	// data() {
-	// 	console.log(this.data);
-	// }
 }
