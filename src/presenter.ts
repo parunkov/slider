1aa27ddb56f4ec39a01e4780d1fb29de9f28c6eb
parunkov <parunkov@gmail.com>
2020-02-10 14:14:00 +0500

    Закомментировал init

--- src/presenter.ts
+++ src/presenter.ts
@@ -9,7 +9,7 @@ class Presenter {
 	data: SliderData;
 	view: any;
 	model: any;
-	presenterData: SliderData;
+	// presenterData: SliderData;
 	viewValue: ViewValue;
 	modelValue: ViewValue;
 
@@ -20,25 +20,27 @@ class Presenter {
 	}
 
 	init() {
-		this.presenterData = this.data;
-		this.view = new View(this.presenterData);
-		this.model = new Model(this.presenterData);
-		this.view.data = this.presenterData;
-		this.viewValue = this.view.viewValue;
-		this.modelValue = this.view.viewValue;
-		const setTabValue = (value) => Math.round((this.data.minValue + (this.data.maxValue - this.data.minValue) * value / 100) / this.data.step) * this.data.step;
+		// this.presenterData = this.data;
+		// this.view = new View(this.presenterData);
+		// this.model = new Model(this.presenterData);
+		this.view = new View(this.data);
+		this.model = new Model(this.data);
+		// this.view.data = this.presenterData;
+		// this.viewValue = this.view.viewValue;
+		// this.modelValue = this.view.viewValue;
+		// const setTabValue = (value) => Math.round((this.data.minValue + (this.data.maxValue - this.data.minValue) * value / 100) / this.data.step) * this.data.step;
 		this.view.container.addEventListener('changeValue', (evt) => {
-			this.modelValue.min = +round(setTabValue(this.viewValue.min), this.data.step);
-			this.modelValue.max = +round(setTabValue(this.viewValue.max), this.data.step);
+			// this.modelValue.min = +round(setTabValue(this.viewValue.min), this.data.step);
+			// this.modelValue.max = +round(setTabValue(this.viewValue.max), this.data.step);
 			this.model.observer.dispatchEvent(new CustomEvent('changeValue'));
 		});
 	}
 
 	addListener() {
 		const onMouseMove = (moveEvt) => {
-			const setTabValue = (value) => Math.round((this.data.minValue + (this.data.maxValue - this.data.minValue) * value / 100) / this.data.step) * this.data.step;
-			this.view.viewTabText.min = round(setTabValue(this.view.viewValue.min), this.view.data.step);
-			this.view.viewTabText.max = round(setTabValue(this.view.viewValue.max), this.view.data.step);
+			// const setTabValue = (value) => Math.round((this.data.minValue + (this.data.maxValue - this.data.minValue) * value / 100) / this.data.step) * this.data.step;
+			// this.view.viewTabText.min = round(setTabValue(this.view.viewValue.min), this.view.data.step);
+			// this.view.viewTabText.max = round(setTabValue(this.view.viewValue.max), this.view.data.step);
 			// console.log(this.viewValue);
 		}
 		setMouseHandler(document, onMouseMove);
