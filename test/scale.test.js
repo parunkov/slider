e136355bbd11a3a4b5d5e467a943268bb30fd0ca
parunkov <parunkov@gmail.com>
2020-02-11 16:41:00 +0500

    Сделал тест Scale

--- test/scale.test.js
+++ test/scale.test.js
@@ -4,24 +4,31 @@ import {Scale} from '../src/scale.ts';
 
 describe('Модуль Scale', function() {
 
-	const slider = setFixtures()[0];
-	slider.style.width = '100px';
-	document.body.appendChild(slider);
-	const bar = document.createElement('div');
-	bar.classList.add('ts-slider__bar');
-	slider.append(bar);
+	let slider;
+	let bar;
+	const add = () => {
+		slider = setFixtures()[0];
+		slider.style.width = '100px';
+		document.body.appendChild(slider);
+		bar = document.createElement('div');
+		bar.classList.add('ts-slider__bar');
+		slider.append(bar);
+	}
 
 	it('horizontal - добавляет класс ts-slider__scale-item', function() {
+		add();
 		const scale = new Scale(slider, ['10', '20'], false);
 		expect(bar.querySelector('div')).toHaveClass('ts-slider__scale-item');
 	});
 	it('vertical - добавляет класс ts-slider__scale-item--vertical', function() {
+		add();
 		const scale = new Scale(slider, ['10', '20'], true);
+		// console.log(bar);
 		expect(bar.querySelector('div')).toHaveClass('ts-slider__scale-item--vertical');
 	});
-	it('vertical - добавляет класс ts-slider__scale-item--vertical', function() {
+	it('scale-item - добавлено нужное количество', function() {
+		add();
 		const scale = new Scale(slider, ['10', '20', '30', '40'], false);
-		console.log(bar.querySelectorAll('.ts-slider__scale-item').length);
-		expect(bar.querySelector('div')).toHaveClass('ts-slider__scale-item');
+		expect(bar.querySelectorAll('.ts-slider__scale-item').length).toEqual(4);
 	});	
 });
