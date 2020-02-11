b9d253df751cf7c3c931403614ecec6b82095ad6
parunkov <parunkov@gmail.com>
2020-02-11 14:01:10 +0500

    делаю тесты

--- test/scale.test.js
+++ test/scale.test.js
@@ -2,37 +2,26 @@ import {$} from '../node_modules/jquery/dist/jquery.js';
 import {Scale} from '../src/scale.ts';
 
 
-describe('Scale', function() {
-	it('horizontal', function() {
-		const slider = setFixtures()[0];
-		slider.style.width = '100px';
-		document.body.appendChild(slider);
-		const bar = document.createElement('div');
-		bar.classList.add('ts-slider__bar');
-		slider.append(bar);
-		const scale = new Scale(slider, 4, false);
+describe('Модуль Scale', function() {
+
+	const slider = setFixtures()[0];
+	slider.style.width = '100px';
+	document.body.appendChild(slider);
+	const bar = document.createElement('div');
+	bar.classList.add('ts-slider__bar');
+	slider.append(bar);
+
+	it('horizontal - добавляет класс ts-slider__scale-item', function() {
+		const scale = new Scale(slider, ['10', '20'], false);
 		expect(bar.querySelector('div')).toHaveClass('ts-slider__scale-item');
 	});
-	it('vertical', function() {
-		const slider = setFixtures()[0];
-		slider.style.height = '100px';
-		document.body.appendChild(slider);
-		const bar = document.createElement('div');
-		bar.classList.add('ts-slider__bar');
-		slider.append(bar);
-		const scale = new Scale(slider, 4, true);
+	it('vertical - добавляет класс ts-slider__scale-item--vertical', function() {
+		const scale = new Scale(slider, ['10', '20'], true);
 		expect(bar.querySelector('div')).toHaveClass('ts-slider__scale-item--vertical');
-	});	
-	it('quantity', function() {
-		const slider = setFixtures()[0];
-		slider.style.height = '100px';
-		document.body.appendChild(slider);
-		const bar = document.createElement('div');
-		bar.classList.add('ts-slider__bar');
-		slider.append(bar);
-		const scale1 = new Scale(slider, -1, true);
-		expect(scale1.quantity).toEqual(2);
-		const scale2 = new Scale(slider, 3, true);
-		expect(scale2.quantity).toEqual(3);
+	});
+	it('vertical - добавляет класс ts-slider__scale-item--vertical', function() {
+		const scale = new Scale(slider, ['10', '20', '30', '40'], false);
+		console.log(bar.querySelectorAll('.ts-slider__scale-item').length);
+		expect(bar.querySelector('div')).toHaveClass('ts-slider__scale-item');
 	});	
 });
--- test/set-view-value.test.js
+++ test/set-view-value.test.js
@@ -1,20 +1,7 @@
 import {$} from '../node_modules/jquery/dist/jquery.js';
-import {initControl} from '../src/init-control.ts';
+import {setViewValue} from '../src/set-view-value.ts';
 
 
-describe('initControl', function() {
-	it('horizontal', function() {
-		const wrap = setFixtures()[0];
-		wrap.classList.add('wrap');
-		document.body.appendChild(wrap);
-		initControl('.wrap', false);
-		expect(wrap.querySelector('div')).toHaveClass('ts-slider__container');
-	});
-	it('vertical', function() {
-		const wrap = setFixtures()[0];
-		wrap.classList.add('wrap');
-		document.body.appendChild(wrap);
-		initControl('.wrap', true);
-		expect(wrap.querySelector('div')).toHaveClass('ts-slider__container--vertical');
-	});
+describe('setViwValue', function() {
+	
 });
