13b3fd420c8e171533a7a73d43f5fb20e1bbd08b
parunkov <parunkov@gmail.com>
2020-02-17 14:22:52 +0500

    Исправил ошибку отображения табов

--- src/view.ts
+++ src/view.ts
@@ -56,6 +56,11 @@ class View {
 		if (!this.data.isRange) {
 			this.minToggleElem.hidden = true;
 		}
+
+		if (!this.data.isTab) {
+			this.minTabElem.hidden = true;
+			this.maxTabElem.hidden = true;
+		}
 	}
 
 	setTab() {
@@ -168,118 +173,3 @@ class View {
 }
 
 export {View};
-
-
-
-
-// //old
-
-// import {initViewMarkup} from './init-view-markup.ts';
-// import {Scale} from './scale.ts';
-// import {setViewValue} from './set-view-value.ts';
-// import {Data} from './data.ts';
-// import {setToggleStyle} from './toggle.ts';
-// import {setMouseHandler, round} from './functions.ts';
-// import {markup} from './init-view-markup.ts';
-
-// interface Value {
-// 	min: number;
-// 	max: number;
-// }
-// interface TabText {
-// 	min: string;
-// 	max: string;
-// }
-
-// class View {
-
-// 	data: Data;
-// 	viewValue: Value;
-// 	viewTabText: TabText;
-// 	container: HTMLElement;
-// 	minToggleElem: HTMLElement;
-// 	maxToggleElem: HTMLElement;
-// 	minTabElem: HTMLElement;
-// 	maxTabElem: HTMLElement;
-// 	size: number;
-// 	scale: string[];
-
-// 	constructor(data) {
-// 		this.data = data;
-// 		this.viewTabText = {
-// 			min: '',
-// 			max: ''
-// 		}
-// 		this.scale = [];
-// 		this.initView();
-// 		this.setToggle();
-// 		this.onMoveToggle();
-// 		this.addScale();
-// 		this.changeTab();
-// 		this.changeInput();
-// 	}
-
-// 	initView() {
-// 		initViewMarkup(this.data.wrapId, this.data.isVertical);
-// 		this.container = document.querySelector(this.data.wrapId).querySelector('.ts-slider__container');
-// 		if (this.data.isVertical) {
-// 			this.size = this.container.offsetHeight;
-// 		} else {
-// 			this.size = this.container.offsetWidth;
-// 		}
-// 		this.viewValue = setViewValue(this.container, this.data);
-// 	}
-
-// 	setTab() {
-// 		this.minTabElem.textContent = this.viewTabText.min;
-// 		this.maxTabElem.textContent = this.viewTabText.max;
-// 	}
-
-// 	setToggle() {
-// 		this.minToggleElem = markup(this.container).min;
-// 		this.maxToggleElem = markup(this.container).max;
-// 		this.minTabElem = markup(this.container).minTab;
-// 		this.maxTabElem = markup(this.container).maxTab;
-
-// 		this.container.addEventListener('initValue', () => {
-// 			setToggleStyle(this.minToggleElem, this.viewValue.min * this.size, this.data.isVertical);
-// 			setToggleStyle(this.maxToggleElem, this.viewValue.max * this.size, this.data.isVertical);
-// 			this.setTab();
-// 		});
-// 		// this.viewValue = setViewValue(this.container, this.data);
-// 	}
-
-// 	onMoveToggle() {
-// 		// this.viewValue = setViewValue(this.container, this.data);
-// 		const onMouseMove = (moveEvt) => {
-// 			this.container.dispatchEvent(new CustomEvent('moveToggle'));
-// 			// console.log(this.viewValue);
-// 		}
-// 		setMouseHandler(this.container, onMouseMove);
-// 	}
-
-// 	addScale() {
-// 		this.container.addEventListener('initScale', () => {
-// 			if (this.data.isScale) {
-// 				const scale = new Scale(this.container, this.scale, this.data.isVertical);
-// 			}
-// 		});
-// 	}
-
-// 	changeTab() {
-// 		this.container.addEventListener('changeTab', () => {
-// 			this.setTab();
-// 		});
-// 	}
-// 	changeInput() {
-// 		this.container.addEventListener('changeInput', () => {
-// 			// console.log(222);
-// 			// console.log(this.viewValue);
-// 			setToggleStyle(this.minToggleElem, this.viewValue.min * this.size, this.data.isVertical);
-// 			setToggleStyle(this.maxToggleElem, this.viewValue.max * this.size, this.data.isVertical);
-// 			this.setTab();
-// 		});
-// 	}
-// }
-
-// export {View, Value, TabText};
