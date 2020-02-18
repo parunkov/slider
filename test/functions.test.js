20b2359c45f91c47439af1df927dcbeaf1655ee1
parunkov <parunkov@gmail.com>
2020-02-18 08:51:22 +0500

    дополнил тесты функций

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,5 +1,5 @@
 import {$} from '../node_modules/jquery/dist/jquery.js';
-import {round, setLimit} from '../src/functions.ts';
+import {round, setLimit, toPrecent, toValue, setRangeStyle} from '../src/functions.ts';
 
 describe('Функция round', function() {
 	it('Округляет до шага', function() {
@@ -13,5 +13,32 @@ describe('Функция setLimit', function() {
 		expect(setLimit(5, 10, 15)).toEqual(10);
 		expect(setLimit(20, 10, 15)).toEqual(15);
 		expect(setLimit(12, 10, 15)).toEqual(12);
+	});
+});
+
+describe('Функция toPrecent', function() {
+	it('Пересчитывает значение в процент', function() {
+		expect(toPrecent(30, 0, 100)).toEqual(0.3);
+	});
+});
+
+describe('Функция toValue', function() {
+	it('Пересчитывает процент в значение', function() {
+		expect(toValue(0.3, 0, 100)).toEqual(30);
+		expect(toValue(0.5, 100, 200)).toEqual(150);
+	});
+});
+
+describe('Функция setRangeStyle', function() {
+	const div = setFixtures()[0];
+	it('Устанавливает стиль range для вертикального слайдера', function() {
+		setRangeStyle(div, 10, 100, true);
+		expect(div.style.top).toEqual('10px');
+		expect(div.style.height).toEqual('90px');
+	});
+	it('Устанавливает стиль range для горизонтального слайдера', function() {
+		setRangeStyle(div, 10, 100, false);
+		expect(div.style.left).toEqual('10px');
+		expect(div.style.width).toEqual('90px');
 	});
 });
