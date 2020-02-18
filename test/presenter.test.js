5738b40b41820842a0437e33ec2f5caf16ad63b2
parunkov <parunkov@gmail.com>
2020-02-18 08:34:49 +0500

    Исправил тест презентера

--- test/presenter.test.js
+++ test/presenter.test.js
@@ -1,76 +1,89 @@
-// import {$} from '../node_modules/jquery/dist/jquery.js';
-// import {toView, toModel, Presenter} from '../src/presenter.ts';
+import {$} from '../node_modules/jquery/dist/jquery.js';
+import {toView, toModel, Presenter} from '../src/presenter.ts';
 
-// describe('Модуль Presenter', function() {
+describe('Модуль Presenter', function() {
 
-// 	let sliderWrap;
-// 	let presenter;
-// 	const data = {
-// 		wrapId: '#presenterwrapid',
-// 		isArray: false,
-// 		array: [],
-// 		step: 1,
-// 		minValue: 0,
-// 		maxValue: 10,
-// 		minToggleValue: 3,
-// 		maxToggleValue: 5,
-// 		isRange: true,
-// 		isVertical: false,
-// 		isTab: true,
-// 		isScale: true,
-// 		scaleQuantity: 5
-// 	}
-// 	const add = () => {
-// 		sliderWrap = setFixtures()[0];
-// 		sliderWrap.setAttribute('id','presenterwrapid');
-// 		document.body.appendChild(sliderWrap);
-// 		presenter = new Presenter(data);
-// 	}
+	let sliderWrap;
+	let presenter;
+	let input1;
+	let input2;
+	const data = {
+		wrapId: '#presenterwrapid',
+		minInputId: 'page__input-11',
+		maxInputId: 'page__input-12',
+		isArray: false,
+		array: [],
+		step: 1,
+		minValue: 0,
+		maxValue: 10,
+		minToggleValue: 3,
+		maxToggleValue: 5,
+		isRange: true,
+		isVertical: false,
+		isTab: true,
+		isScale: true,
+		scaleQuantity: 5
+	}
+	const add = () => {
+		sliderWrap = setFixtures()[0];
+		sliderWrap.setAttribute('id','presenterwrapid');
+		document.body.appendChild(sliderWrap);
 
-// 	it('Функция toView пересчитывает value', function() {
-// 		add();
-// 		expect(toView(30, 0, 100)).toEqual(0.3);
-// 		sliderWrap.remove();
-// 	});
-// 	it('Функция toModel пересчитывает value', function() {
-// 		add();
-// 		expect(toModel(0.3, 0, 100)).toEqual(30);
-// 		sliderWrap.remove();
-// 	});
-// 	it('Создает view', function() {
-// 		add();
-// 		expect(presenter.view).toBeDefined;
-// 		sliderWrap.remove();
-// 	});
-// 	it('Создает model', function() {
-// 		add();
-// 		expect(presenter.model).toBeDefined;
-// 		sliderWrap.remove();
-// 	});
-// 	it('Задает текст для Tab в виде', function() {
-// 		add();
-// 		expect(presenter.view.viewTabText).toEqual({min: '3', max: '5'});
-// 		sliderWrap.remove();
-// 	});
+		input1 = setFixtures()[0];
+		input1.setAttribute('id','page__input-11');
+		document.body.appendChild(input1);
+
+		input2 = setFixtures()[0];
+		input2.setAttribute('id','page__input-12');
+		document.body.appendChild(input2);
+
+		presenter = new Presenter(data);
+	}
+
+	// it('Функция toPrecent пересчитывает value', function() {
+	// 	add();
+	// 	expect(toPrecent(30, 0, 100)).toEqual(0.3);
+	// 	sliderWrap.remove();
+	// });
+	// it('Функция toModel пересчитывает value', function() {
+	// 	add();
+	// 	expect(toModel(0.3, 0, 100)).toEqual(30);
+	// 	sliderWrap.remove();
+	// });
+	it('Создает view', function() {
+		add();
+		expect(presenter.view).toBeDefined;
+		sliderWrap.remove();
+	});
+	it('Создает model', function() {
+		add();
+		expect(presenter.model).toBeDefined;
+		sliderWrap.remove();
+	});
+	it('Задает текст для Tab в виде', function() {
+		add();
+		expect(presenter.view.tabText).toEqual({min: '3', max: '5'});
+		sliderWrap.remove();
+	});
 	
-// 	it('Устанавливает presenter.data', function() {
-// 		add();
-// 		expect(presenter.data).toBe(data);
-// 		sliderWrap.remove();
-// 	});
-// 	it('Устанавливает presenter.value', function() {
-// 		add();
-// 		expect(presenter.value).toEqual({min: 3, max: 5});
-// 		sliderWrap.remove();
-// 	});
-// 	it('Устанавливает шкалу в виде', function() {
-// 		add();
-// 		expect(presenter.view.scale.length).toEqual(5);
-// 		sliderWrap.remove();
-// 	});	
-// 	// it('', function() {
-// 	// 	add();
-// 	// 	expect().to;
-// 	// 	sliderWrap.remove();
-// 	// });
-// });
+	it('Устанавливает presenter.data', function() {
+		add();
+		expect(presenter.data).toBe(data);
+		sliderWrap.remove();
+	});
+	it('Устанавливает presenter.value', function() {
+		add();
+		expect(presenter.value).toEqual({min: 3, max: 5});
+		sliderWrap.remove();
+	});
+	it('Устанавливает шкалу в виде', function() {
+		add();
+		expect(presenter.view.scale.length).toEqual(5);
+		sliderWrap.remove();
+	});	
+	// it('', function() {
+	// 	add();
+	// 	expect().to;
+	// 	sliderWrap.remove();
+	// });
+});
