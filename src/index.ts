60a7a09a1a571836a870c09543926fc90db08ca8
parunkov <parunkov@gmail.com>
2020-02-20 14:59:16 +0500

    Создал методы плагина

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import {Presenter} from './presenter.ts';
 
 (function($) {
-	$.fn.tsSlider = function(options) {
-		const id: string = '#' + this.prop('id');
-		const wrap = document.querySelector(id);
-		const presenter = new Presenter(options, wrap);
+	const methods = {
+		init: function(options) {
+			// console.log('init');
+			const id: string = '#' + this.prop('id');
+			const wrap = document.querySelector(id);
+			const presenter = new Presenter(options, wrap);
+		},
+		change: function(options) {
+			console.log('change');
+		},
+		arrayChange: function(options) {
+			console.log('arrayChange');
+		}
+	}
+
+	$.fn.tsSlider = function(options, method?) {
+		// const id: string = '#' + this.prop('id');
+		// const wrap = document.querySelector(id);
+		// const presenter = new Presenter(options, wrap);
+		 if ( methods[method] ) {
+		      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		    } else if ( typeof method === 'object' || ! method ) {
+		      return methods.init.apply( this, arguments );
+		    } else {
+		      $.error( 'Метод с именем ' +  method + ' не существует для jQuery.tsSlider' );
+		    } 
+
 		return this;
 	}
 })(jQuery);
