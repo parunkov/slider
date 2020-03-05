674f47d0ad6ab5e29d42e1372e3049322d3901d8
parunkov <parunkov@gmail.com>
2020-03-05 08:27:16 +0500

    Сделал изменения первого слайдера

--- dist/service.js
+++ dist/service.js
@@ -1,24 +1,46 @@
-const form1 = document.querySelector('.page__form--1');
-const id1 = '#' + $('#ts-slider__wrap-1').tsSlider(sliderData1).prop('id');
-const data1 = window[id1].data;
-const setControl = (form, data) => {
-	const minInput = form.querySelector('.page__input--1');
-	const maxInput = form.querySelector('.page__input--2');
-	const stepInput = form.querySelector('.page__input--3');
-	const scaleInput = form.querySelector('.page__input--4');
-	const rangeCheckbox = form.querySelector('.page__checkbox--1');
-	const verticalCheckbox = form.querySelector('.page__checkbox--2');
-	const tabCheckbox = form.querySelector('.page__checkbox--3');
-	const scaleCheckbox = form.querySelector('.page__checkbox--4');
+(function() {
+	const form1 = document.querySelector('.page__form--1');
+	const id1 = '#ts-slider__wrap-1';
+	const data1 = window[id1].data;
+	const setControl = (id, form, data) => {
+		const minInput = form.querySelector('.page__input--1');
+		const maxInput = form.querySelector('.page__input--2');
+		const stepInput = form.querySelector('.page__input--3');
+		const scaleInput = form.querySelector('.page__input--4');
+		const rangeCheckbox = form.querySelector('.page__checkbox--1');
+		const verticalCheckbox = form.querySelector('.page__checkbox--2');
+		const tabCheckbox = form.querySelector('.page__checkbox--3');
+		const scaleCheckbox = form.querySelector('.page__checkbox--4');
+		const btn = form.querySelector('.page__button');
 
-	minInput.value = data.minValue;
-	maxInput.value = data.maxValue;
-	stepInput.value = data.step;
-	scaleInput.value = data.scaleQuantity;
-	rangeCheckbox.checked = data.isRange;
-	verticalCheckbox.checked = data.isVertical;
-	tabCheckbox.checked = data.isTab;
-	scaleCheckbox.checked = data.isScale;
+		minInput.value = data.minValue;
+		maxInput.value = data.maxValue;
+		stepInput.value = data.step;
+		scaleInput.value = data.scaleQuantity;
+		rangeCheckbox.checked = data.isRange;
+		verticalCheckbox.checked = data.isVertical;
+		tabCheckbox.checked = data.isTab;
+		scaleCheckbox.checked = data.isScale;
 
-}
-setControl(form1, data1);
+		const onBtnClick = (evt) => {
+			evt.preventDefault();
+			// console.log(id);
+			let data = new Object();
+			data.minValue = +minInput.value;
+			data.maxValue = +maxInput.value;
+			data.step = +stepInput.value;
+			data.scaleQuantity = +scaleInput.value;
+			data.isRange = rangeCheckbox.checked;
+			data.isVertical = verticalCheckbox.checked;
+			data.isTab = tabCheckbox.checked;
+			data.isScale =scaleCheckbox.checked;
+			console.log(data);
+
+			$(id).tsSlider(data, 'change');
+		}
+
+		btn.addEventListener('click', onBtnClick);
+
+	}
+	setControl(id1, form1, data1);
+})();
